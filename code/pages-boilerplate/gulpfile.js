02b1fad2d84b9706e2fb4cfe693b99c3eb3abad8
wangxi <136365575@qq.com>
2020-12-23 14:40:50 +0800

    update gulpfile.js

--- code/pages-boilerplate/gulpfile.js
+++ code/pages-boilerplate/gulpfile.js
@@ -11,64 +11,92 @@ const plugins = loadPlugins() // 一次性加载所有gulp-*的插件
 const del = require('del')
 
 const bs = require('browser-sync')
+const cwd = process.cwd()
+
+let config = {
+    build: {
+        src: 'src',
+        dist: 'dist',
+        tmp: 'tmp',
+        public: 'public',
+        paths: {
+            styles: 'assets/styles/*.scss',
+            scripts: 'assets/scripts/*.js',
+            pages: '*.html',
+            images: 'assets/images/**',
+            fonts: 'assets/fonts/**'
+        }
+    },
+    data: {}
+}
+
+try {
+    const localConf = require(`${cwd}/pages.config.js`)
+    config = Object.assign({}, config, localConf)
+} catch(e) {}
+
 
 const style = () => {
-    return src('src/assets/styles/*.scss', { base: 'src'}) // 添加一个src option
+    return src(config.build.paths.styles, { base: config.build.src, cwd: config.build.src}) // 添加一个src option
         .pipe(plugins.sass({
             outputStyle: 'expanded'
         }))
-        .pipe(dest('tmp'))
-        // .pipe(bs.reload({stream: true})) // 也可以用这种方法，就不要再bs.init里面用files
+        .pipe(dest(config.build.tmp))
+        .pipe(bs.reload({stream: true})) // 也可以用这种方法，就不要再bs.init里面用files
 }
 
 const script = () => {
-    return src('src/assets/scripts/*.js', { base: 'src'})
+    return src(config.build.paths.scripts, { base: config.build.src, cwd: config.build.src})
         .pipe(plugins.babel({
             presets: ['@babel/preset-env']
         }))
-        .pipe(dest('tmp'))
+        .pipe(dest(config.build.tmp))
+        .pipe(bs.reload({stream: true})) // 也可以用这种方法，就不要再bs.init里面用files
 }
 
 const page = () => {
-    return src('src/**/*.html', { base: 'src'})
-        .pipe(plugins.swig())
-        .pipe(dest('tmp'))
+    return src(config.build.paths.pages, { base: config.build.src, cwd: config.build.src})
+        .pipe(plugins.swig(config.data))
+        .pipe(dest(config.build.tmp))
+        .pipe(bs.reload({stream: true})) // 也可以用这种方法，就不要再bs.init里面用files
 }
 
 const image = () => {
-    return src('src/assets/images/**', { base: 'src'})
+    return src(config.build.paths.images, { base: config.build.src, cwd: config.build.src})
         .pipe(plugins.imagemin())
-        .pipe(dest('dist'))
+        .pipe(dest(config.build.dist))
 }
 
 const font = () => {
-    return src('src/assets/fonts/*', { base: 'src'})
+    return src(config.build.paths.fonts, { base: config.build.src, cwd: config.build.src})
         .pipe(plugins.imagemin())
-        .pipe(dest('dist'))
+        .pipe(dest(config.build.dist))
 }
 
 const extra = () => {
-    return src('public/**', { base: 'public'})
-        .pipe(dest('dist'))
+    return src('**', { base: config.build.public, cwd: config.build.public})
+        .pipe(dest(config.build.dist))
 }
 
 const clean = () => {
-    return del(['dist','tmp'])
+    return del([config.build.dist,config.build.tmp])
 }
 
 const serve = () => {
-    watch('src/assets/styles/*.scss', style)
-    watch('src/assets/scripts/*.js', script)
-    watch('src/**/*.html', page) 
+    watch(config.build.paths.styles, { cwd: config.build.src }, style)
+    watch(config.build.paths.scripts, { cwd: config.build.src }, script)
+    watch(config.build.paths.pages, { cwd: config.build.src }, page) 
 
     // 当图片或字体发生改变的时候，reload浏览器，而不是进行构建
-    watch(['src/assets/images/**','src/assets/fonts/*', 'public/**'], bs.reload) 
+    watch([config.build.paths.images,config.build.paths.fonts], { cwd: config.build.src }, bs.reload) 
+
+    watch('**', { cwd: config.build.public }, bs.reload)
 
     bs.init({
         port: 3000,
-        files: 'tmp/**', // 监听文件变化
+        // files: 'tmp/**', // 监听文件变化
         server: {
-            baseDir: ['tmp', 'src', 'public'], // 如果第一个找不到，就回到后面的目录中寻找
+            baseDir: [config.build.tmp, config.build.src, config.build.public], // 如果第一个找不到，就回到后面的目录中寻找
             routes: {
                 // /node_modules开头的请求都被转发到node_modules文件夹中
                 '/node_modules': 'node_modules' 
@@ -78,12 +106,12 @@ const serve = () => {
 }
 
 const useref = () => {
-    return src('tmp/*.html', {base: 'tmp'})
-        .pipe(plugins.useref({ searchPath: ['tmp', '.']})) // 根据注释，将外部引用的依赖进行合并
+    return src(config.build.paths.pages, {base: config.build.tmp, cwd: config.build.tmp})
+        .pipe(plugins.useref({ searchPath: [config.build.tmp, '.']})) // 根据注释，将外部引用的依赖进行合并
         .pipe(plugins.if(/\.js$/, plugins.uglify())) // 压缩js，css，html
         .pipe(plugins.if(/\.css$/, plugins.cleanCss()))
         .pipe(plugins.if(/\.html$/, plugins.htmlmin({ collapseWhitespace: true, minifyCSS: true, minifyJS: true})))
-        .pipe(dest('dist'))
+        .pipe(dest(config.build.dist))
 }
 
 const compile = parallel(style, script, page)
